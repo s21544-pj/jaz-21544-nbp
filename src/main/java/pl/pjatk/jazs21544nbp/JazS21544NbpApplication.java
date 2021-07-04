5e75ca1fba4527656927a436d9281e5823264cec
warhir <krzyzopolski.mariusz@gmail.com>
2021-07-04 16:48:28 +0200

    init

---
+++ src/main/java/pl/pjatk/jazs21544nbp/JazS21544NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs21544nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS21544NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS21544NbpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pjatk/jazs21544nbp/JazS21544NbpApplicationTests.java
@@ -0,0 +1,13 @@
+//package pl.pjatk.jazs21544nbp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class JazS21544NbpApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
